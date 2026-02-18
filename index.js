d434682a7c1a5eb5291752ea110b699b802388a3
Matthew <killerfoxgaming08712@gmail.com>
2026-02-18 14:48:53 -0500

    information can be correctly entered but key value as a whole gets deleted, slight design overhauls

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 function encode() {
-    const recieving = document.getElementById("entryfield");
-    const key = document.getElementById("key");
-    console.log(recieving.textContent)
-    console.log(key.textContent)
+    let safe = true
+    const recieving = document.getElementsByClassName("inputted")[0];
+    const codekey = document.getElementById("encodekey");
+    const error = document.getElementById("warning");
+    if (codekey.value = " ") {
+        console.log(recieving.value);
+        console.log("error", codekey.value);
+        error.style.visibility = "visible";
+        safe = false;
+    } else {
+        console.log("good", codekey.value);
+        error.style.visibility = "hidden";
+        safe = true;
+    }
+    let encoded = document.getElementsByClassName("inputted")[1];
+    /* take alphabet and cut it off by the selected key letter, shift letters to the end by adding split letters to the remaining letters*/
+    /*https://pages.mtu.edu/~shene/NSF-4/Tutorial/VIG/Vig-Base.html*/
+    if (safe) {
+       encoded.value = recieving.value; 
+    }
 };
