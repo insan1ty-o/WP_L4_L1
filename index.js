9d6b508afbbc01fe62055c4553c90a7213db2abc
Matthew <killerfoxgaming08712@gmail.com>
2026-02-18 10:11:14 -0500

    text boxes aligned correctly, bare basics of js done for a starting point, error system in implementation

---
+++ index.js
@@ -0,0 +1,6 @@
+function encode() {
+    const recieving = document.getElementById("entryfield");
+    const key = document.getElementById("key");
+    console.log(recieving.textContent)
+    console.log(key.textContent)
+};
